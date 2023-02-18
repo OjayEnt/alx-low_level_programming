14f8d9ab69b398066497e5b3bf510b07f25fdb8e
OjayEnt <oyero28@gmail.com>
2023-02-18 03:41:20 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - multilingual puzzle
+ *
+ * Return: it is zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
