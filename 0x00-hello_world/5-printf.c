f116188b7b8282a3757cf1d868086ddbe20893bd
OjayEnt <oyero28@gmail.com>
2023-02-18 04:07:49 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - with proper grammar
+ *
+ * Return: this is zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("\n");
+	return (0);
+}
