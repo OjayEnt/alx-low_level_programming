2c7ce3b9b76dfdb3e2b9948a48fa1fec8caf4fa3
OjayEnt <oyero28@gmail.com>
2023-02-21 19:30:05 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_sign - function that print sign
+ *
+ * @n: parameter for numbers
+ *
+ * Return: 1 if positive
+ * and 0 if zero
+ * and -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
