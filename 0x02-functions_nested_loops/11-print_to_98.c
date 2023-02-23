53fca63625da0c38dde388ca7bac90d6b11e72ae
OjayEnt <oyero28@gmail.com>
2023-02-23 09:32:59 -0800

    print natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,9 +13,9 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n >= 0 && n <= 97)
+	if (n >= 0 && n <= 98)
 	{
-		for (i = n ; i <= 97 ; i++)
+		for (i = n ; i <= 98 ; i++)
 		{
 			printf("%d", i);
 			if (i != 98)
