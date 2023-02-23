d9c18348da93195851159b56b8c891832844037a
OjayEnt <oyero28@gmail.com>
2023-02-23 15:53:17 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _isupper - upper case
+ * @c: the alphabet
+ * Return: it is 0
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+		printf("%c: 1\n", c);
+	else
+		printf("%c: 0\n", c);
+	return (0);
+}
