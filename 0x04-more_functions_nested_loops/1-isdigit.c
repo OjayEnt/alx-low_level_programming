55752b6f2b927ecacbb2d02b34f19b2b9f7a0bcd
OjayEnt <oyero28@gmail.com>
2023-02-23 16:36:51 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - the digit
+ *@c: parameter for digit
+ *Return: it is 0
+ */
+
+int _isdigit(int c)
+{
+	if (_isdigit(c))
+		printf("%c: 1", c);
+	else
+		printf("%c: 0", c);
+	return (0);
+}
