38b6b3a6fee2e9f3ccde98ef6d76517383b37e47
OjayEnt <oyero28@gmail.com>
2023-03-14 09:37:22 -0700

    static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - upper case
+ * @c: the alphabet
+ * Return: 1 if c is upper
+ * 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - the digit
+ *@c: parameter for digit
+ *Return: 1 if it is a digit
+ *0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
