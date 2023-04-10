6ab41ebcb3f73644d2da2b1742d75e017a1fbcf0
OjayEnt <oyero28@gmail.com>
2023-04-10 04:10:59 -0700

    function that prints sentenses

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,11 +4,10 @@ void first(void) __attribute__ ((constructor));
 
 /**
  * first - prints a sentence before the main
- * function execute
+ * function is executed
  */
-
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!,\n");
+	printf("I bore my house upon my back!\n");
 }
