d74913a21313d6501d4e3f71defdb808abc70aaa
OjayEnt <oyero28@gmail.com>
2023-04-16 04:44:48 -0700

    function that compares two strings and returns 1

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,10 +1,13 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * wildcmp - Compare strings
- * @s1: pointer to string params
- * @s2: pointer to string params
- * Return: 0
+ * @s1: pointer to string
+ * @s2: pointer to string
+ *
+ * Return: zero
  */
 
 int wildcmp(char *s1, char *s2)
@@ -28,4 +31,3 @@ int wildcmp(char *s1, char *s2)
 	}
 	return (0);
 }
-
